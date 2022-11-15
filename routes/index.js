e68fb28cf2233345b01d22888cc00be9fa371dbf
Sierra Godfrey <srgodfrey001@converse.edu>
2022-11-15 12:17:48 -0500

    final hw 5

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', formdata: undefined });
+});
+
+/* POST home page. */
+router.post('/', function(req, res, next) {
+  res.render('index', { title: 'Express', formdata: req.body});
 });
 
 module.exports = router;
